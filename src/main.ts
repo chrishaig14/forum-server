dc9401c5255cfa5166e1209f562cbd53b5de50bd
chrishaig14 <chrishaig14@gmail.com>
2019-09-26 02:01:20 -0300

    Change database url to mongo atlas cluster

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import * as logic from './logic';
 const {app, setLogic} = require('../src/app');
 
 (async () => {
-    const url = 'mongodb://127.0.0.1:27017';
-    const dbName = 'forum_test';
+    // const url = 'mongodb://127.0.0.1:27017';
+    // const dbName = 'forum_test';
+    const url = 'mongodb+srv://chris:ClXgGz5Wn5DYrka1@cluster0-rdvz2.mongodb.net/test?retryWrites=true&w=majority';
+    const dbName = 'forum';
     await logic.connectToDb(url, dbName);
     await logic.db.collection('counters').insertOne({name: 'questionId', value: 0});
     await logic.db.collection('counters').insertOne({name: 'answerId', value: 0});
